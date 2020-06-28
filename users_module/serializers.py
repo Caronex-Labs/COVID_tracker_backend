8fe30c568f701bf39883a22c496ef623fb5f1378
SameeranB <sameeranbandishti@ieee.org>
2020-06-29 02:04:12 +0530

    Overwrite Login Serializer to allow for phone numbers

--- users_module/serializers.py
+++ users_module/serializers.py
@@ -1,11 +1,15 @@
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
 from phonenumber_field.serializerfields import PhoneNumberField
 from rest_auth.registration.serializers import RegisterSerializer
 from rest_auth.serializers import LoginSerializer
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 # Uncomment the following serializers to set up custom serializers for login and registration. You will also have to
 # uncomment lines in the settings.py file in order to use custom serializers.
 from users_module.models import User, Daily
+
+UserModel = get_user_model()
 
 
 class CustomLoginSerializer(LoginSerializer):
@@ -13,6 +17,24 @@ class CustomLoginSerializer(LoginSerializer):
     email = None
     phone = PhoneNumberField(required=True)
     password = serializers.CharField(style={'input_type': 'password'})
+
+    def validate(self, attrs):
+        phone = attrs.get('phone')
+        password = attrs.get('password')
+
+        user = authenticate(self.context['request'], phone=phone, password=password)
+
+        # Did we get back an active user?
+        if user:
+            if not user.is_active:
+                msg = _('User account is disabled.')
+                raise exceptions.ValidationError(msg)
+        else:
+            msg = _('Unable to log in with provided credentials.')
+            raise exceptions.ValidationError(msg)
+
+        attrs['user'] = user
+        return attrs
 
 
 class CustomRegisterSerializer(RegisterSerializer):
@@ -46,7 +68,6 @@ class UserDailySerializer(serializers.ModelSerializer):
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
-
     daily_records = UserDailySerializer(many=True)
 
     class Meta:
