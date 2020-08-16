855781fa45fd995c0eb9c9650f7cbd4efb220679
Mayank Shah <mayankshah1614@gmail.com>
2020-08-16 19:12:15 +0530

    Delete project
    Signed-off-by: Mayank Shah <mayankshah1614@gmail.com>

--- users_module/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersModuleConfig(AppConfig):
-    name = 'users_module'
--- users_module/router.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from users_module.views import UserViewSet, PatientViewSet
-
-UserAppRouter = routers.DefaultRouter()
-
-UserAppRouter.register('users', UserViewSet, basename='Users')
-UserAppRouter.register('patients', PatientViewSet, basename='Patients')
